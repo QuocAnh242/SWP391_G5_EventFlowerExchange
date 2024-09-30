ab7ad1c80aa9c50cc08fead7b201e7683549a573
Tran Ho Quoc Anh <wasasi241224@gmail.com>
2024-09-30 09:19:30 +0700

    Change File Name, Adding FE files

---
+++ SWP391_G5_EventFlowerExchange_BE/LoginAPI/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/repository/INotificationsRepository.java
@@ -0,0 +1,7 @@
+package com.SWP391_G5_EventFlowerExchange.LoginAPI.repository;
+
+import com.SWP391_G5_EventFlowerExchange.LoginAPI.entity.Notifications;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface INotificationsRepository extends JpaRepository<Notifications, Integer> {
+}
---
+++ SWP391_G5_EventFlowerExchange_BE/LoginAPI/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/service/CatetoryService.java
@@ -0,0 +1,45 @@
+package com.SWP391_G5_EventFlowerExchange.LoginAPI.service;
+
+import com.SWP391_G5_EventFlowerExchange.LoginAPI.entity.Category;
+import com.SWP391_G5_EventFlowerExchange.LoginAPI.repository.ICatetoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class CatetoryService implements ICatetoryService{
+    @Autowired
+    private ICatetoryRepository icatetoryRepository;
+    @Override
+    public List<Category> getAllCategories() {
+        return icatetoryRepository.findAll();
+    }
+
+    @Override
+    public Category insertCategory(Category category) {
+        return icatetoryRepository.save(category);
+    }
+
+    @Override
+    public Category updateCategory(int catetoryId, Category category) {
+        Category cate = icatetoryRepository.getById(catetoryId);
+        if(cate != null){
+            cate.setFlowerType(category.getFlowerType());
+            cate.setDescription(category.getDescription());
+            cate.setImageUrl(category.getImageUrl());
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteCategory(int catetoryId) {
+        icatetoryRepository.deleteById(catetoryId);
+    }
+
+    @Override
+    public Optional<Category> getCategoryById(int catetoryId) {
+        return icatetoryRepository.findById(catetoryId);
+    }
+}
