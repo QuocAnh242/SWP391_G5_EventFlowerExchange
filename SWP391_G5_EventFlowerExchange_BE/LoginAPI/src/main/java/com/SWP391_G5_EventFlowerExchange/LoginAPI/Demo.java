fe4483c4313a1e907293793283d9cd1a6978cb98
Tran Ho Quoc Anh <wasasi241224@gmail.com>
2024-10-01 10:10:41 +0700

    Fix Imports Services, Controllers

---
+++ SWP391_G5_EventFlowerExchange_BE/LoginAPI/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/Demo.java
@@ -0,0 +1,15 @@
+package com.SWP391_G5_EventFlowerExchange.LoginAPI;
+
+public class Demo {
+
+    public  boolean  isPrimeNumber(int input)  {
+        if (input < 2) {
+            return false;
+        }
+        for  (int i = 2; i < input; i++)  {
+            if  (input % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
