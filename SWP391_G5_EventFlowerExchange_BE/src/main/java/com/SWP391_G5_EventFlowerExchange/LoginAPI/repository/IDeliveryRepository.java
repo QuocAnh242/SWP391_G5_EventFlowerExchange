51e2ed928f88298c8044e1eeac74f8bbae18e4b2
NguyenHao1009 <haonvse184938@fpt.edu.vn>
2024-10-12 21:34:30 +0700

    Delete SWP391_G5_EventFlowerExchange_BE/src directory

--- SWP391_G5_EventFlowerExchange_BE/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/repository/IDeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SWP391_G5_EventFlowerExchange.LoginAPI.repository;
-
-import com.SWP391_G5_EventFlowerExchange.LoginAPI.entity.Delivery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IDeliveryRepository extends JpaRepository<Delivery, Integer> {
-
-}
