796565e78027cead24e3aee1fed06807049f7f1c
NguyenHao1009 <haonvse184938@fpt.edu.vn>
2024-10-20 00:03:50 +0700

    Delete src directory

--- src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SWP391_G5_EventFlowerExchange.LoginAPI.repository;
-
-import com.SWP391_G5_EventFlowerExchange.LoginAPI.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-}
-
