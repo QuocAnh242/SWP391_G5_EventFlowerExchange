48775c91131b3679c62a6ac05c26274836206bf3
NguyenHao1009 <haonvse184938@fpt.edu.vn>
2024-10-25 00:59:40 +0700

    Add files via upload

--- SWP391_G5_EventFlowerExchange_BE/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/configuration/VNPayConfig.java
+++ SWP391_G5_EventFlowerExchange_BE/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/configuration/VNPayConfig.java
@@ -9,7 +9,7 @@ public class VNPayConfig {
     public static final String vnp_TmnCode = "2FCLB0LY";
     public static final String vnp_HashSecret = "6MSE4RMSB3BF3ZV72ESVL3BJE82YFMHH";
     public static final String vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";  // URL VNPay sandbox
-    public static final String vnp_ReturnUrl = "http://localhost:8080/identity/payments/";  // URL trả về khi thanh toán thành công
+    public static final String vnp_ReturnUrl = "http://localhost:3000/success-page";  // URL trả về khi thanh toán thành công
 
     public static final String vnp_RefundUrl="https://sandbox.vnpayment.vn/merchant_webapi/api/transaction"; //URL VNPays sandbox refund
 
