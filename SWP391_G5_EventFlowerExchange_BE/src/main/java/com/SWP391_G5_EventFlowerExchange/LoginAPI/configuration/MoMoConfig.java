18a72447fcaab5db9825098e1cf29ee856cba45d
Datphan1802 <157185612+Datphan1802@users.noreply.github.com>
2024-10-25 23:54:32 +0700

    modify MOMO pay

---
+++ SWP391_G5_EventFlowerExchange_BE/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/configuration/MoMoConfig.java
@@ -0,0 +1,10 @@
+package com.SWP391_G5_EventFlowerExchange.LoginAPI.configuration;
+
+public class MoMoConfig {
+    public static final String MOMO_URL = "https://test-payment.momo.vn/gw_payment/transactionProcessor";
+    public static final String PARTNER_CODE = "YOUR_PARTNER_CODE";
+    public static final String ACCESS_KEY = "YOUR_ACCESS_KEY";
+    public static final String SECRET_KEY = "YOUR_SECRET_KEY";
+    public static final String RETURN_URL = "YOUR_RETURN_URL";
+    public static final String NOTIFY_URL = "YOUR_NOTIFY_URL";
+}
