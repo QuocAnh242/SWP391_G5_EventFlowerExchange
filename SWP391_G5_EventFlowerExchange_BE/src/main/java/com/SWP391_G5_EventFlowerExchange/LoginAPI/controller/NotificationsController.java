1ef27b5563654918197472e3d92775fdff523f0f
NguyenHao1009 <haonvse184938@fpt.edu.vn>
2024-10-28 23:49:46 +0700

    show noti by userID

--- SWP391_G5_EventFlowerExchange_BE/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/controller/NotificationsController.java
+++ SWP391_G5_EventFlowerExchange_BE/src/main/java/com/SWP391_G5_EventFlowerExchange/LoginAPI/controller/NotificationsController.java
@@ -42,6 +42,7 @@ public class NotificationsController {
         Optional<Notifications> noti= notificationsService.getNotificationsById(id);
         return ResponseEntity.ok(noti);
     }
+
     @GetMapping("/user/{userID}")
     public ResponseEntity<List<Notifications>> getNotiByUserID(@PathVariable int userID) {
         List<Notifications> notifications = notificationsService.getNotificationsByUserId(userID);
